bca8683fbd3c1d80d0ab80dc3e3a803cd63392c9
maxiturchet <71859870+maxiturchet@users.noreply.github.com>
2023-04-27 01:05:29 -0300

    Reto 15

---
+++ Reto15.js
@@ -0,0 +1,24 @@
+function decorateTree(base) { 
+  const dict = {
+  "PP": "P",
+  "BB": "B",
+  "RR": "R",
+  "BP": "R",
+  "PB": "R",
+  "BR": "P",
+  "RB": "P",
+  "PR": "B",
+  "RP": "B"
+}
+ base = base.split(" ")
+  let list = new Array(base.length).fill(base)
+  return list.reduce((total, _) => 
+    total.concat(
+      [new Array(total.at(-1).length - 1).fill("-").map((_, i) => {
+        return dict[total.at(-1).slice(i, i + 2).join("")]
+      })]
+      ), [base]
+  ).slice(0, base.length).map(x => x.join(" ")).reverse()
+}
+
+console.log(decorateTree("B B P R"));
